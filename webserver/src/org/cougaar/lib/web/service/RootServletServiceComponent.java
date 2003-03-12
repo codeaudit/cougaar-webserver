61591766a4881160cdafe41f862ae962a1e8745f
tomlinso <tomlinso>
2003-03-12 18:10:28 +0000

    * org/cougaar/lib/web/service/RootServletServiceComponent.java: Bug #2658 -- Components incompatible with automatic service revocation feature Test for null service before trying to use it.

--- webserver/src/org/cougaar/lib/web/service/RootServletServiceComponent.java
+++ webserver/src/org/cougaar/lib/web/service/RootServletServiceComponent.java
@@ -168,7 +168,11 @@ implements Component
   }
 
   public void setNodeControlService(NodeControlService ncs) {
-    this.sb = ncs.getRootServiceBroker();
+    if (ncs == null) {
+      // Revocation
+    } else {
+      this.sb = ncs.getRootServiceBroker();
+    }
   }
 
   public void setParameter(Object o) {
